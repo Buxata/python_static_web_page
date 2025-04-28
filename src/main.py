3072ca0b43955db8c4bf6658c3a9fbd68c150b6f
Viktor Vidolov <viktor.vidolov@nuvolo.com>
2025-04-28 23:30:50 +0300

    B: adding inline nodes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
+from textnode import TextNode
 print("hello world")
+
+test = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+print (test)
--- src/textnode.py
+++ src/textnode.py
@@ -2,8 +2,23 @@ from enum import Enum
 
 class TextType(Enum):
     NORMAL = "normal"
-    BOLD= "**Bold**"
-    ITALIC = "_Italic_"
-    CODE = "`Code`"
-    LINK = "[anchor](url)"
-    IMAGE = "![alt](url)"
+    BOLD= "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type:TextType, url=None):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
