b5b83fa67d719f21709dc4fd02d2f8223df7c064
buxata <vidolarez@gmail.com>
2025-05-03 18:28:15 +0300

    E: added converted the textnode to leafnode

---
+++ src/nodes/parentnode.py
@@ -0,0 +1,12 @@
+from src.nodes.htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children):
+        super().__init__(tag, None, children, props=None)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Tag is required")
+        if not self.children:
+            raise ValueError("Children are required")
+        return f"<{self.tag}>{''.join(list(map(lambda child: child.to_html(), self.children)))}</{self.tag}>"
